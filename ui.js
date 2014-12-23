a80c912f784ebd5d7efbaab7c403690486027bdb
gliesedev <info@gliese.io>
2014-12-23 21:36:02 +0400

    Implement stream system in filehandler

--- ui.js
+++ ui.js
@@ -91,17 +91,51 @@ FS.EventHandlers = {};
 
 // Simplifies some of the repetitive code for making an event handler that does a file insert
 FS.EventHandlers.insertFiles = function cfsInsertFiles(collection, options) {
+  
+  // Assign options to options paramater or default to an empty object
   options = options || {};
   var afterCallback = options.after;
   var metadataCallback = options.metadata;
-
+  
+  // Create a function insertFilesHandler which we will return 
   function insertFilesHandler(event, template) {
+    // Use the .eachFile utility function to iterate over an array of files.
     FS.Utility.eachFile(event, function (file) {
+      // Pass variable f to FS.file constructor function 
       var f = new FS.File(file);
+      // if a metadataCallback exists then use the extend utility function to add the metadata to the 'f' file object
       metadataCallback && FS.Utility.extend(f, metadataCallback(f));
-      collection.insert(f, afterCallback);
+      // Insert a stream into the files collection - using the supplied metadata to set the queueId,fileStreamId and 
+      // the streamSize for the file 
+      Streams.Files.insert({
+          queueId: f.queueId,
+          fileStreamId: f.fileStreamId,
+          uploadedSize: 0,
+          streamSize: f.data.blob.size
+      });
+
+      // Query the database to see if a Queue stream of the Id supplied in the metadata already exists
+      var queue = Streams.Queues.findOne({queueId: f.queueId});
+      // if a queue is found:
+      if (queue) {
+         // Update the queue we found - incrementing the total streamSize to include the current file object
+         Streams.Queues.update(queue._id, {$inc: {streamSize: f.data.blob.size}});
+      } else {
+        // else: insert a brand new Stream into the queues collection with an initial streamSize equal to the 
+        // size of the current file
+          Streams.Queues.insert(
+              { queueId: f.queueId,
+                uploadedSize: 0,
+                streamSize: f.data.blob.size
+              }
+          )
+      }
+      // Once we are done with all the stream handling, insert the file into the collection 
+        collection.insert(f, afterCallback);
     });
-  };
+  }
 
   return insertFilesHandler;
 };
+
+
